cdf27db3e3ce95ab91695971ee313f51579add47
Jitgitbit <rowowolf@gmail.com>
2020-11-05 13:15:45 +0100

    main logic done

--- dictionary.py
+++ dictionary.py
@@ -2,4 +2,11 @@ import json
 
 data = json.load(open("data.json"))
 
-print(data["smog"])
+def translate(word):
+	if word in data:
+		return data[word]
+
+
+word = input("Enter the word that you want to search here: ")
+output = translate(word)
+print(output)
